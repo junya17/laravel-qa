af2af52669b40d90c78a3b3148e49f57e9079e98
谷口純也 <taniguchi@taniguchi.local>
2018-11-06 16:49:24 +0900

    Prepuplate users question tables using model factories and seed

--- app/User.php
+++ app/User.php
@@ -32,8 +32,4 @@ class User extends Authenticatable
         return $this->hasMany(Question::class);
     } 
 
-    public function setTitleAttribute($value){
-        $this->attributes['title'] = $value;
-        $this->attributes['siug'] = str_slug($value);
-    }
 }
